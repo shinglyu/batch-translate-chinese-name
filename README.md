01460cb186fd72145a8315cf81b5b94c5c973067
Shing Lyu <slyu@mozilla.com>
2017-08-17 10:55:48 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Batch convert Chinese name to English (Pinyin)
+
+## Usage
+
+```
+pip3 install pinyin
+python3 convert input.txt > output.txt
+```
+
+## Input file format
+Input file must be a list of names, each name is a separate line. You can mix Chinese names with English names, but English names will not be modified.
+
+```
+王小明
+李大華
+Jason Smith
+```
+
+## Options
+
+```
+optional arguments:
+  -h, --help       show this help message and exit
+  --rearrange, -r  Rearrange into <first-name> <last name> format
+```
+
+* `--rearrange` or `-r`
+  * With `-r`: 王小明 => Xiao-ming Wang
+  * Without `-r`: 王小明 => Wan xiao ming
---
+++ convert.py
@@ -0,0 +1,48 @@
+import pinyin
+import argparse
+
+
+def isEnglish(s):
+    try:
+        s.encode('ascii')
+    except UnicodeEncodeError:
+        return False
+    else:
+        return True
+
+def toPinyin(s, rearrange=False):
+    s = s.strip()
+    if isEnglish(s):
+        return s
+
+    chars = []
+    if rearrange:
+        firstname=s[1:]
+        lastname=s[0]
+        chars.append(pinyin.get(firstname, format="strip", delimiter="-").strip("-"))
+        chars.append(pinyin.get(lastname, format="strip", delimiter="-"))
+    else:
+        chars.append(pinyin.get(s, format="strip", delimiter=" "))
+    return " ".join(map(lambda x: x.capitalize(), chars))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('filename', help='The input filename')
+    parser.add_argument('--rearrange', '-r', help='Rearrange into <first-name> <last name> format', action='store_true')
+
+    args = parser.parse_args()
+    with open(args.filename, 'r') as f:
+        lines = f.readlines()
+    output = []
+    for line in lines:
+        output.append(toPinyin(line, args.rearrange).strip())
+
+    for line in output:
+        print(line)
+
+if __name__ == "__main__":
+    main()
+
+
+
